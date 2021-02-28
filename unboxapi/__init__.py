b54619e40cc2fc91277d0d700c72902c5b1cca5f
Rishab Ramanathan <rishramanathan@gmail.com>
2021-02-28 18:32:02 -0500

    [UNB-60] upload to firebase storage using firebase auth credentials

--- unboxapi/__init__.py
+++ unboxapi/__init__.py
@@ -1,24 +1,51 @@
 import os
+import getpass
+import tarfile
+import pyrebase
 import bentoml
+from bentoml.saved_bundle.bundler import _write_bento_content_to_dir
+from bentoml.utils.tempdir import TempDirectory
 
 from .template import create_template_model
 
 
 class UnboxClient(object):
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self):
+        self.authenticate()
 
     def add(self, function, model):
         bento_service = create_template_model("sklearn", "text")
         bento_service.pack('model', model)
         bento_service.pack('function', function)
 
-        self.upload(bento_service, "Tg0T6QuoZ5baPz5Hbw3fvz8tvGw1")
+        with TempDirectory() as temp_dir:
+            _write_bento_content_to_dir(bento_service, temp_dir)
+            with TempDirectory() as tarfile_dir:
+                file_name = f'{bento_service.name}.tar'
+                tarfile_path = f'{tarfile_dir}/{file_name}'
+                with tarfile.open(tarfile_path, mode="w:gz") as tar:
+                    tar.add(temp_dir, arcname=bento_service.name)
+                self.upload(
+                    f"users/{self.user['localId']}/models/{file_name}", tarfile_path)
 
-    @staticmethod
-    def upload(bento_service, user_id):
-        # TODO: Authenticate with the user provided API key
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/rishabramanathan/projects/unbox/serviceAccount.json'
+    def upload(self, remote_path, file_path):
+        storage = self.firebase.storage()
+        storage.child(remote_path).put(file_path, self.user['idToken'])
 
-        bentoml.save_to_dir(bento_service,
-                            f'gs://unbox-ai.appspot.com/{user_id}/models')
+    def authenticate(self):
+        config = {
+            "apiKey": "AIzaSyAKlGQOmXTjPQhL1Uvj-Jr-_jUtNWmpOgs",
+            "authDomain": "unbox-ai.firebaseapp.com",
+            "databaseURL": "https://unbox-ai.firebaseio.com",
+            "storageBucket": "unbox-ai.appspot.com"
+        }
+
+        self.firebase = pyrebase.initialize_app(config)
+
+        # Get a reference to the auth service
+        auth = self.firebase.auth()
+
+        # Log the user in
+        email = input("What is your Unbox email?")
+        password = getpass.getpass("What is your Unbox password?")
+        self.user = auth.sign_in_with_email_and_password(email, password)
