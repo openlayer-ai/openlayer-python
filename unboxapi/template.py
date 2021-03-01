0899117971ddd5be04f9eab8f708afe6c153c825
Rishab Ramanathan <rishramanathan@gmail.com>
2021-02-28 23:03:36 -0500

    fixed the issue with overspecification of template models

--- unboxapi/template.py
+++ unboxapi/template.py
@@ -1,49 +1,55 @@
-from typing import List
-from bentoml import env, artifacts, api, BentoService
-from bentoml.frameworks import sklearn, pytorch, tensorflow, transformers
-from bentoml.service.artifacts.common import PickleArtifact
-from bentoml.adapters import JsonInput, DataframeInput
-from bentoml.types import JsonSerializable, InferenceTask
+import textwrap
 
 
 modelTypes = {
-    "sklearn": sklearn.SklearnModelArtifact,
-    "pytorch": pytorch.PytorchModelArtifact,
-    "tensorflow": tensorflow.TensorflowSavedModelArtifact,
-    "transformers": transformers.TransformersModelArtifact
+    "sklearn": "SklearnModelArtifact",
+    "pytorch": "PytorchModelArtifact",
+    "tensorflow": "TensorflowSavedModelArtifact",
+    "transformers": "TransformersModelArtifac"
 }
 
 
 def create_template_model(model_type, input_type):
-    @env(infer_pip_packages=True)
-    @artifacts([modelTypes[model_type]('model'), PickleArtifact('function')])
-    class TemplateModel(BentoService):
+    # return TemplateModel([modelTypes[model_type]('model'), PickleArtifact('function')])
+    with open('template_model.py', 'w') as python_file:
+        file_contents = f"""from typing import List
+        from bentoml import env, artifacts, api, BentoService
+        from bentoml.frameworks.{model_type} import {modelTypes[model_type]}
+        from bentoml.service.artifacts.common import PickleArtifact
+        from bentoml.adapters import JsonInput, DataframeInput
+        from bentoml.types import JsonSerializable, InferenceTask
 
-        @api(input=DataframeInput(
-            orient="records",
-            columns=["text"],
-            dtype={"text": "str"},
-        ), batch=True)
-        def batch(self, df):
-            text = df['text'].tolist()
-            return self.artifacts.function(
-                self.artifacts.model,
-                text
-            )
+        @env(infer_pip_packages=True)
+        @artifacts([{modelTypes[model_type]}('model'), PickleArtifact('function')])
+        class TemplateModel(BentoService):
 
-        @api(input=JsonInput(), batch=True)
-        def predict(self, parsed_json_list: List[JsonSerializable], tasks: List[InferenceTask]):
-            text = []
-            for json, task in zip(parsed_json_list, tasks):
-                if "text" in json:
-                    text.append(json['text'])
-                else:
-                    task.discard(http_status=400,
-                                 err_msg="input json must contain `text` field")
+            @api(input=DataframeInput(
+                orient="records",
+                columns=["text"],
+                dtype={{"text": "str"}},
+            ), batch=True)
+            def batch(self, df):
+                text = df['text'].tolist()
+                return self.artifacts.function(
+                    self.artifacts.model,
+                    text
+                )
 
-            return self.artifacts.function(
-                self.artifacts.model,
-                text
-            )
+            @api(input=JsonInput(), batch=True)
+            def predict(self, parsed_json_list: List[JsonSerializable], tasks: List[InferenceTask]):
+                text = []
+                for json, task in zip(parsed_json_list, tasks):
+                    if "text" in json:
+                        text.append(json['text'])
+                    else:
+                        task.discard(http_status=400,
+                                    err_msg="input json must contain `text` field")
 
+                return self.artifacts.function(
+                    self.artifacts.model,
+                    text
+                )
+        """
+        python_file.write(textwrap.dedent(file_contents))
+    from template_model import TemplateModel
     return TemplateModel()
