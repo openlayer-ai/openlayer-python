1de3b841b72a10a3659a2d1658f167f5c2dd797b
Rishab Ramanathan <rishramanathan@gmail.com>
2021-03-09 13:31:57 -0500

    Test function for error analysis & descriptions for dataset and model

--- unboxapi/lib/network.py
+++ unboxapi/lib/network.py
@@ -17,11 +17,13 @@ class FlaskAPI:
                                 user_id: str,
                                 dataset_id: str,
                                 name: str,
+                                description: str,
                                 id_token: str):
         data = {
             'dataset_id': dataset_id,
             'id_token': id_token,
             'name': name,
+            'description': description,
             'user_id': user_id
         }
         return self.post(endpoint='/dataset', data=data)
@@ -30,11 +32,13 @@ class FlaskAPI:
                               user_id: str,
                               model_id: str,
                               name: str,
+                              description: str,
                               id_token: str):
         data = {
             'model_id': model_id,
             'id_token': id_token,
             'name': name,
+            'description': description,
             'user_id': user_id
         }
         return self.post(endpoint='/upload_model_metadata', data=data)
@@ -43,6 +47,23 @@ class FlaskAPI:
         data = {'name': name, 'id_token': id_token}
         file = open(file_path, 'rb')
         return self.post(endpoint='/dataset', data=data, file=file)
+
+    def _test_associate_model_dataset(self,
+                                      id_token: str,
+                                      model_id: str,
+                                      dataset_id: str,
+                                      user_id='ytGD2XvoGPSaippqWhAmi5V8mHT2',
+                                      text_col='text',
+                                      label_col='polarity'):
+        data = {
+            'id_token': id_token,
+            'user_id': user_id,
+            'model_id': model_id,
+            'dataset_id': dataset_id,
+            'text_col': text_col,
+            'label_col': label_col
+        }
+        return self.post(endpoint='/associate', data=data)
 
 
 class FirebaseAPI:
