842e4413bfd6635e836fb5ba4e1742ee94273e66
Rishab Ramanathan <rishramanathan@gmail.com>
2021-03-15 23:38:37 -0700

    Fixes

--- unboxapi/__init__.py
+++ unboxapi/__init__.py
@@ -22,69 +22,93 @@ class UnboxClient(object):
         self.firebase_api = FirebaseAPI(email=email, password=password)
 
     def _test_associate(self, model_id: str, dataset_id: str, user_id: str):
-        ''' This is just for testing.'''
+        """ This is just for testing."""
         return self.flask_api._test_associate_model_dataset(
-            id_token=self.firebase_api.user['idToken'],
+            id_token=self.firebase_api.user["idToken"],
             model_id=model_id,
             dataset_id=dataset_id,
-            user_id=user_id)
+            user_id=user_id,
+        )
 
     def _test_add_test_suite(self, run_id: str, user_id: str, test_config: any):
-        ''' This is just for testing.'''
+        """ This is just for testing."""
         return self.flask_api._test_add_test_suite(
-            id_token=self.firebase_api.user['idToken'],
+            id_token=self.firebase_api.user["idToken"],
             run_id=run_id,
             user_id=user_id,
-            test_config=test_config)
+            test_config=test_config,
+        )
 
-    def add_model(self, function, model, name: str, description: str, model_type: str = 'sklearn'):
+    def add_model(
+        self, function, model, name: str, description: str, model_type: str = "sklearn"
+    ):
         bento_service = create_template_model(model_type)
-        bento_service.pack('model', model)
-        bento_service.pack('function', function)
+        bento_service.pack("model", model)
+        bento_service.pack("function", function)
 
         with TempDirectory() as temp_dir:
             _write_bento_content_to_dir(bento_service, temp_dir)
 
             with TempDirectory() as tarfile_dir:
                 model_id = str(uuid.uuid1())
-                tarfile_path = f'{tarfile_dir}/{model_id}'
+                tarfile_path = f"{tarfile_dir}/{model_id}"
 
-                with tarfile.open(tarfile_path, mode='w:gz') as tar:
+                with tarfile.open(tarfile_path, mode="w:gz") as tar:
                     tar.add(temp_dir, arcname=bento_service.name)
 
-                user_id = self.firebase_api.user['localId']
-                remote_path = f'users/{user_id}/models/{model_id}'
+                user_id = self.firebase_api.user["localId"]
+                remote_path = f"users/{user_id}/models/{model_id}"
                 self.firebase_api.upload(remote_path, tarfile_path)
 
                 # Now set the metadata via request to our Flask API
-                id_token = self.firebase_api.user['idToken']
-                response = self.flask_api.upload_model_metadata(user_id,
-                                                                model_id,
-                                                                name,
-                                                                description,
-                                                                id_token)
+                id_token = self.firebase_api.user["idToken"]
+                response = self.flask_api.upload_model_metadata(
+                    user_id, model_id, name, description, id_token
+                )
         return response
 
-    def add_dataset(self, file_path: str, name: str, description: str, label_column_name: str, text_column_name: str):
+    def add_dataset(
+        self,
+        file_path: str,
+        name: str,
+        description: str,
+        label_column_name: str,
+        text_column_name: str,
+    ):
         # For now, let's upload straight to Firebase Storage from here
-        user_id = self.firebase_api.user['localId']
+        user_id = self.firebase_api.user["localId"]
         dataset_id = str(uuid.uuid1())
-        remote_path = f'users/{user_id}/datasets/{dataset_id}'
+        remote_path = f"users/{user_id}/datasets/{dataset_id}"
         self.firebase_api.upload(remote_path, file_path)
 
         # And then set the metadata via request to our Flask API
-        id_token = self.firebase_api.user['idToken']
-        response = self.flask_api.upload_dataset_metadata(user_id,
-                                                          dataset_id,
-                                                          name,
-                                                          description,
-                                                          label_column_name,
-                                                          text_column_name,
-                                                          id_token)
+        id_token = self.firebase_api.user["idToken"]
+        response = self.flask_api.upload_dataset_metadata(
+            user_id,
+            dataset_id,
+            name,
+            description,
+            label_column_name,
+            text_column_name,
+            id_token,
+        )
         return response.json()
 
-    def add_dataframe(self, df: pd.DataFrame, name: str, description: str, label_column_name: str, text_column_name: str):
+    def add_dataframe(
+        self,
+        df: pd.DataFrame,
+        name: str,
+        description: str,
+        label_column_name: str,
+        text_column_name: str,
+    ):
         with tempfile.TemporaryDirectory() as tmp_dir:
             dataset_file_path = os.path.join(tmp_dir, str(uuid.uuid1()))
             df.to_csv(dataset_file_path, index=False)
-            return self.add_dataset(dataset_file_path, name, description, label_column_name, text_column_name)
+            return self.add_dataset(
+                dataset_file_path,
+                name,
+                description,
+                label_column_name,
+                text_column_name,
+            )
--- unboxapi/lib/network.py
+++ unboxapi/lib/network.py
@@ -4,93 +4,86 @@ import getpass
 
 
 class FlaskAPI:
-
     def __init__(self):
-        self.url = 'http://localhost:5000'
+        self.url = "http://0.0.0.0:8080"
 
-    def post(self, endpoint: str = '/', data: any = None, file: any = None):
-        return requests.post(self.url + endpoint,
-                             json=data,
-                             files=file and {'file': file})
+    def post(self, endpoint: str = "/", data: any = None, file: any = None):
+        return requests.post(
+            self.url + endpoint, json=data, files=file and {"file": file}
+        )
 
-    def upload_dataset_metadata(self,
-                                user_id: str,
-                                dataset_id: str,
-                                name: str,
-                                description: str,
-                                label_column_name: str,
-                                text_column_name: str,
-                                id_token: str):
+    def upload_dataset_metadata(
+        self,
+        user_id: str,
+        dataset_id: str,
+        name: str,
+        description: str,
+        label_column_name: str,
+        text_column_name: str,
+        id_token: str,
+    ):
         data = {
-            'datasetId': dataset_id,
-            'idToken': id_token,
-            'name': name,
-            'description': description,
-            'labelColumnName': label_column_name,
-            'textColumnName': text_column_name,
-            'userId': user_id
+            "datasetId": dataset_id,
+            "idToken": id_token,
+            "name": name,
+            "description": description,
+            "labelColumnName": label_column_name,
+            "textColumnName": text_column_name,
+            "userId": user_id,
         }
-        return self.post(endpoint='/api/dataset/upload_metadata', data=data)
+        return self.post(endpoint="/api/dataset/upload_metadata", data=data)
 
-    def upload_model_metadata(self,
-                              user_id: str,
-                              model_id: str,
-                              name: str,
-                              description: str,
-                              id_token: str):
+    def upload_model_metadata(
+        self, user_id: str, model_id: str, name: str, description: str, id_token: str
+    ):
         data = {
-            'modelId': model_id,
-            'idToken': id_token,
-            'name': name,
-            'description': description,
-            'userId': user_id
+            "modelId": model_id,
+            "idToken": id_token,
+            "name": name,
+            "description": description,
+            "userId": user_id,
         }
-        return self.post(endpoint='/api/model/upload_metadata', data=data)
+        return self.post(endpoint="/api/model/upload_metadata", data=data)
 
     # def upload_dataset(self, file_path: str, name: str, id_token: str):
     #     data = {'name': name, 'idToken': id_token}
     #     file = open(file_path, 'rb')
     #     return self.post(endpoint='/api/dataset/upload_dataset', data=data, file=file)
 
-    def _test_associate_model_dataset(self,
-                                      id_token: str,
-                                      model_id: str,
-                                      dataset_id: str,
-                                      user_id: str):
+    def _test_associate_model_dataset(
+        self, id_token: str, model_id: str, dataset_id: str, user_id: str
+    ):
         data = {
-            'idToken': id_token,
-            'userId': user_id,
-            'modelId': model_id,
-            'datasetId': dataset_id
+            "idToken": id_token,
+            "userId": user_id,
+            "modelId": model_id,
+            "datasetId": dataset_id,
         }
-        return self.post(endpoint='/api/run/create_run', data=data)
+        return self.post(endpoint="/api/run/create_run", data=data)
 
-    def _test_add_test_suite(self,
-                             id_token: str,
-                             run_id: str,
-                             user_id: str,
-                             test_config: any):
+    def _test_add_test_suite(
+        self, id_token: str, run_id: str, user_id: str, test_config: any
+    ):
         data = {
-            'idToken': id_token,
-            'userId': user_id,
-            'runId': run_id,
-            'testConfig': test_config
+            "idToken": id_token,
+            "userId": user_id,
+            "runId": run_id,
+            "testConfig": test_config,
         }
-        return self.post(endpoint='/api/run/add_test_suite', data=data)
+        return self.post(endpoint="/api/run/add_test_suite", data=data)
 
 
 class FirebaseAPI:
-
     def __init__(self, email: str = None, password: str = None):
         if not email or not password:
-            email = input('What is your Unbox email?')
-            password = getpass.getpass('What is your Unbox password?')
+            email = input("What is your Unbox email?")
+            password = getpass.getpass("What is your Unbox password?")
 
         config = {
-            'apiKey': 'AIzaSyAKlGQOmXTjPQhL1Uvj-Jr-_jUtNWmpOgs',
-            'authDomain': 'unbox-ai.firebaseapp.com',
-            'databaseURL': 'https://unbox-ai.firebaseio.com',
-            'storageBucket': 'unbox-ai.appspot.com'
+            "apiKey": "AIzaSyAKlGQOmXTjPQhL1Uvj-Jr-_jUtNWmpOgs",
+            "authDomain": "unbox-ai.firebaseapp.com",
+            "databaseURL": "https://unbox-ai.firebaseio.com",
+            "storageBucket": "unbox-ai.appspot.com",
         }
 
         # Initialize Pyrebase instance
@@ -104,5 +97,5 @@ class FirebaseAPI:
 
     def upload(self, remote_path: str, file_path: str):
         storage = self.firebase.storage()
-        id_token = self.user['idToken']
+        id_token = self.user["idToken"]
         storage.child(remote_path).put(file_path, id_token)
