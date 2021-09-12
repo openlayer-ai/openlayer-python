288c97f1e99540d2f2cd1c1d102e0a93e61036d3
Rishab Ramanathan <rishramanathan@gmail.com>
2021-09-12 14:57:23 -0400

    update api url

--- unboxapi/api.py
+++ unboxapi/api.py
@@ -9,7 +9,7 @@ from tqdm.utils import CallbackIOWrapper
 
 from .exceptions import ExceptionMap, UnboxException
 
-# UNBOX_ENDPOINT = "https://api-staging.tryunbox.ai/api"
+# UNBOX_ENDPOINT = "https://api-staging.unbox.ai/api"
 UNBOX_ENDPOINT = "http://localhost:8080/api"
 UNBOX_STORAGE_PATH = os.path.expanduser("~/unbox/unbox-onpremise/userStorage")
 
