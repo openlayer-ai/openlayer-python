0f2e3faa80edf1d6a04805847cba490965430d04
Rishab Ramanathan <rishramanathan@gmail.com>
2022-03-31 13:41:48 -0700

    Closes UNB-1854 Prevent retries when client sdk version doesn't match

--- unboxapi/api.py
+++ unboxapi/api.py
@@ -18,7 +18,7 @@ UNBOX_STORAGE_PATH = os.path.expanduser("~/unbox/unbox-onpremise/userStorage")
 # Parameters for HTTP retry
 HTTP_TOTAL_RETRIES = 3  # Number of total retries
 HTTP_RETRY_BACKOFF_FACTOR = 2  # Wait 1, 2, 4 seconds between retries
-HTTP_STATUS_FORCE_LIST = [408, 429] + list(range(500, 531))
+HTTP_STATUS_FORCE_LIST = [408, 429] + list(range(500, 504)) + list(range(506, 531))
 HTTP_RETRY_ALLOWED_METHODS = frozenset({"GET", "POST"})
 
 CLIENT_METADATA = {"version": __version__}
