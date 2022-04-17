8b0a2577978b0f3cd6f232b4cc1ccde45388311b
Rishab Ramanathan <rishramanathan@gmail.com>
2022-04-17 14:50:42 -0700

    Add PUT to retry allow list

--- unboxapi/api.py
+++ unboxapi/api.py
@@ -19,7 +19,7 @@ UNBOX_STORAGE_PATH = os.path.expanduser("~/unbox/unbox-onpremise/userStorage")
 HTTP_TOTAL_RETRIES = 3  # Number of total retries
 HTTP_RETRY_BACKOFF_FACTOR = 2  # Wait 1, 2, 4 seconds between retries
 HTTP_STATUS_FORCE_LIST = [408, 429] + list(range(500, 504)) + list(range(506, 531))
-HTTP_RETRY_ALLOWED_METHODS = frozenset({"GET", "POST"})
+HTTP_RETRY_ALLOWED_METHODS = frozenset({"GET", "PUT", "POST"})
 
 CLIENT_METADATA = {"version": __version__}
 
