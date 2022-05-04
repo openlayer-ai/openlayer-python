2feeeeb6bdefdb8009dc19cf112ca84690e034c0
gustavocidornelas <gustavocidornelas@gmail.com>
2022-05-03 21:43:37 -0300

    added css styling file similar to pandas and reduced logo size

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,6 +116,7 @@ html_logo = "_static/img/unbox.svg"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/style.css"]
 
 html_css_files = [
     "css/logo.css",
