96aaefcebb4113066eb2c80e339df42e2d5043c1
Rishab Ramanathan <rishramanathan@gmail.com>
2022-05-22 18:35:32 -0400

    Bump version

--- unboxapi/version.py
+++ unboxapi/version.py
@@ -1,3 +1,3 @@
 # Define the SDK version here so that the interal package can have access to this value.
 # See https://stackoverflow.com/questions/2058802
-__version__ = "0.1.1"
+__version__ = "0.1.2"
