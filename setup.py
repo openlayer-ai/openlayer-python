84c0b1141d44bf96861a0aa7f0c0f4b35b01f454
Rishab Ramanathan <rishramanathan@gmail.com>
2022-05-22 17:41:47 -0400

    Closes UNB-2029 Add to PyPI (and does a github actions deploy)

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python3
 
-import pathlib
-from distutils.util import convert_path
+import setuptools
 
-import pkg_resources
-from setuptools import find_packages, setup
-
-with pathlib.Path("requirements.txt").open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement in pkg_resources.parse_requirements(requirements_txt)
-    ]
-
-main_ns = {}
-ver_path = convert_path("unboxapi/version.py")
-with open(ver_path) as ver_file:
-    exec(ver_file.read(), main_ns)
-
-setup(
-    name="unboxapi",
-    version=main_ns["__version__"],
-    description="The official Python client library for Unbox, the Testing and Debugging Platform for AI",
-    url="https://github.com/unboxai/unboxapi-python-client",
-    author="Unbox",
-    license="BSD",
-    packages=find_packages(exclude=["js", "node_modules", "tests"]),
-    python_requires=">=3.7",
-    install_requires=install_requires,
-    include_package_data=True,
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setuptools.setup()
