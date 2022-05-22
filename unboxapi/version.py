356ce89132863375963c41cc6d7d8107cbe96c55
Rishab Ramanathan <rishramanathan@gmail.com>
2022-05-22 18:02:07 -0400

    Fixes UNB-2029 by changing version attr access in setup.cfg

--- unboxapi/version.py
+++ unboxapi/version.py
@@ -1,3 +1,3 @@
-# Define the SDK version here so that both the setup.py and the interal package can have access to this value.
+# Define the SDK version here so that the interal package can have access to this value.
 # See https://stackoverflow.com/questions/2058802
 __version__ = "0.1.0"
