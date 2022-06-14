7051b30471dd64ff6858c15e1fb4f81ab3dd214c
Parthib Samadder <parthib@unbox.ai>
2022-06-14 13:07:43 -0700

    Added support for interacting with projects; model and dataset creation can now only happen in a project - UNB-1570

---
+++ unboxapi/projects.py
@@ -0,0 +1,59 @@
+from .models import Model
+from .datasets import Dataset
+
+class Project:
+    """An object containing information about a project on the Unbox platform."""
+
+    def __init__(self, json, upload, subscription_plan, client):
+        self._json = json
+        self.id = json["id"]
+        self.upload = upload
+        self.subscription_plan = subscription_plan
+        self.client = client
+
+    def __getattr__(self, name):
+        if name in self._json:
+            return self._json[name]
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute {name}")
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return f"Project(id={self.id})"
+
+    def __repr__(self):
+        return f"Project({self._json})"
+
+    def to_dict(self):
+        """Returns object properties as a dict.
+
+        Returns
+        -------
+        Dict with object properties.
+        """
+        return self._json
+
+    def add_model(
+        self,
+        *args,
+        **kwargs,
+    ) -> Model:
+        kwargs["project_id"] = self.id
+        return self.client.add_model(*args, **kwargs)
+
+    def add_dataset(
+        self,
+        *args,
+        **kwargs,
+    ) -> Dataset:
+        kwargs["project_id"] = self.id
+        return self.client.add_dataset(*args, **kwargs)
+
+    def add_dataframe(
+        self,
+        *args,
+        **kwargs
+    ) -> Dataset:
+        kwargs["project_id"] = self.id
+        return self.client.add_dataframe(*args, **kwargs)
