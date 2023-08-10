a0816f44f83864f4cd14c04941993913ea8ad2aa
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2023-08-10 18:33:04 -0300

    Completes OPEN-4810 Replace prompt template with new method for prompt construction on the Python client

--- openlayer/model_runners/ll_model_runners.py
+++ openlayer/model_runners/ll_model_runners.py
@@ -6,7 +6,7 @@ Module with the concrete LLM runners.
 import logging
 import warnings
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Dict, List, Optional, Union
 
 import anthropic
 import cohere
@@ -31,11 +31,21 @@ class LLModelRunner(base_model_runner.ModelRunnerInterface, ABC):
         """
         if (
             self.model_config.get("input_variable_names") is None
-            or self.model_config.get("prompt_template") is None
+            or self.model_config.get("prompt") is None
         ):
-            raise ValueError(
-                "Input variable names and prompt template must be provided."
-            )
+            raise ValueError("Input variable names and prompt must be provided.")
+
+        for message in self.model_config["prompt"]:
+            if message.get("role") is None or message.get("content") is None:
+                raise ValueError(
+                    "Every item in the 'prompt' list must contain "
+                    "'role' and 'content' keys."
+                )
+            if message["role"] not in ["system", "user", "assistant"]:
+                raise ValueError(
+                    "The 'role' key in the 'prompt' list must be one of "
+                    "'system', 'user', or 'assistant'."
+                )
 
     def _run_in_memory(self, input_data_df: pd.DataFrame) -> pd.DataFrame:
         """Runs the input data through the model in memory."""
@@ -46,12 +56,13 @@ class LLModelRunner(base_model_runner.ModelRunnerInterface, ABC):
             input_variables_dict = input_data_row[1][
                 self.model_config["input_variable_names"]
             ].to_dict()
-            input_text = self._inject_prompt_template(
+            injected_prompt = self._inject_prompt(
                 input_variables_dict=input_variables_dict
             )
+            llm_input = self._get_llm_input(injected_prompt)
 
             try:
-                model_outputs.append(self._get_llm_output(input_text=input_text))
+                model_outputs.append(self._get_llm_output(llm_input))
             except Exception as exc:
                 model_outputs.append(
                     f"[Error] Could not get predictions for row: {exc}"
@@ -60,7 +71,7 @@ class LLModelRunner(base_model_runner.ModelRunnerInterface, ABC):
         self.logger.info("Successfully ran data through the model!")
         return pd.DataFrame({"predictions": model_outputs})
 
-    def _inject_prompt_template(self, input_variables_dict: dict) -> str:
+    def _inject_prompt(self, input_variables_dict: dict) -> List[Dict[str, str]]:
         """Injects the input variables into the prompt template.
 
         The prompt template must contain handlebar expressions.
@@ -73,11 +84,22 @@ class LLModelRunner(base_model_runner.ModelRunnerInterface, ABC):
         """
         self.logger.info("Injecting input variables into the prompt template...")
         compiler = pybars.Compiler()
-        formatter = compiler.compile(self.model_config["prompt_template"].strip())
-        return formatter(input_variables_dict)
+
+        injected_prompt = []
+        for message in self.model_config["prompt"]:
+            formatter = compiler.compile(message["content"].strip())
+            injected_prompt.append(
+                {"role": message["role"], "content": formatter(input_variables_dict)}
+            )
+        return injected_prompt
 
     @abstractmethod
-    def _get_llm_output(self, input_text: str) -> str:
+    def _get_llm_input(self, injected_prompt: List[Dict[str, str]]) -> Union[List, str]:
+        """Implements the logic to prepare the input for the language model."""
+        pass
+
+    @abstractmethod
+    def _get_llm_output(self, llm_input: Union[List, str]) -> str:
         """Implements the logic to get the output from the language model for
         a given input text."""
         pass
@@ -125,12 +147,28 @@ class AnthropicModelRunner(LLModelRunner):
             )
             self.model_config["model_parameters"]["max_tokens_to_sample"] = 200
 
-    def _get_llm_output(self, input_text: str) -> str:
+    def _get_llm_input(self, injected_prompt: List[Dict[str, str]]) -> str:
+        """Prepares the input for Anthropic's generate model."""
+        llm_input = ""
+        for message in injected_prompt:
+            if message["role"] == "assistant":
+                llm_input += f"{anthropic.AI_PROMPT} {message['content']} "
+            elif message["role"] == "user" or message["role"] == "system":
+                llm_input += f"{anthropic.HUMAN_PROMPT} {message['content']} "
+            else:
+                raise ValueError(
+                    "Message role must be either 'assistant', 'user', or 'system' for Anthropic LLMs. "
+                    f"Got: {message['role']}"
+                )
+        llm_input += f"{anthropic.AI_PROMPT}"
+        return llm_input
+
+    def _get_llm_output(self, llm_input: str) -> str:
         """Gets the output from Cohere's generate model
         for a given input text."""
         return self.anthropic_client.completions.create(
             model=self.model_config.get("model", "claude-2"),
-            prompt=f"{anthropic.HUMAN_PROMPT} {input_text} {anthropic.AI_PROMPT}",
+            prompt=llm_input,
             **self.model_config.get("model_parameters", {}),
         )["completion"]
 
@@ -161,12 +199,30 @@ class CohereGenerateModelRunner(LLModelRunner):
         if self.model_config.get("model_parameters") is None:
             warnings.warn("No model parameters specified. Using default parameters.")
 
-    def _get_llm_output(self, input_text: str) -> str:
+    def _get_llm_input(self, injected_prompt: List[Dict[str, str]]) -> str:
+        """Prepares the input for Cohere's generate model."""
+        llm_input = ""
+        for message in injected_prompt:
+            if message["role"] == "system":
+                llm_input += f"S: {message['content']} \n"
+            elif message["role"] == "assistant":
+                llm_input += f"A: {message['content']} \n"
+            elif message["role"] == "user":
+                llm_input += f"U: {message['content']} \n"
+            else:
+                raise ValueError(
+                    "Message role must be either 'system', 'assistant' or 'user'. "
+                    f"Got: {message['role']}"
+                )
+        llm_input += "A:"
+        return llm_input
+
+    def _get_llm_output(self, llm_input: str) -> str:
         """Gets the output from Cohere's generate model
         for a given input text."""
         return self.cohere_client.generate(
             model=self.model_config.get("model", "command"),
-            prompt=input_text,
+            prompt=llm_input,
             **self.model_config.get("model_parameters", {}),
         )[0].text
 
@@ -197,11 +253,17 @@ class OpenAIChatCompletionRunner(LLModelRunner):
         if self.model_config.get("model_parameters") is None:
             warnings.warn("No model parameters specified. Using default parameters.")
 
-    def _get_llm_output(self, input_text: str) -> str:
+    def _get_llm_input(
+        self, injected_prompt: List[Dict[str, str]]
+    ) -> List[Dict[str, str]]:
+        """Prepares the input for OpenAI's chat completion model."""
+        return injected_prompt
+
+    def _get_llm_output(self, llm_input: List[Dict[str, str]]) -> str:
         """Gets the output from the OpenAI's chat completion model
         for a given input text."""
         return openai.ChatCompletion.create(
             model=self.model_config.get("model", "gpt-3.5-turbo"),
-            messages=[{"role": "user", "content": input_text}],
+            messages=llm_input,
             **self.model_config.get("model_parameters", {}),
         )["choices"][0]["message"]["content"]
