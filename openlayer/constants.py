e6961fba3579de6965933ad6d02c9d1775f64c25
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2024-01-31 17:50:02 -0300

    Increase max char limit

--- openlayer/constants.py
+++ openlayer/constants.py
@@ -9,7 +9,7 @@ VALID_RESOURCE_NAMES = {"model", "training", "validation", "fine-tuning"}
 OPENLAYER_DIR = os.path.join(os.path.expanduser("~"), ".openlayer")
 
 # -------------------------------- Size limits ------------------------------- #
-MAXIMUM_CHARACTER_LIMIT = 10000
+MAXIMUM_CHARACTER_LIMIT = 50000
 MAXIMUM_TAR_FILE_SIZE = 25  # MB
 
 # ----------------------------------- APIs ----------------------------------- #
