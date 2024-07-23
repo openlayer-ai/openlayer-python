fa3eb5003223b02c36bda486018e8e90349c862c
Rishab Ramanathan <rishramanathan@gmail.com>
2024-07-22 21:31:01 -0700

    feat: upload a batch of inferences

--- src/openlayer/lib/data/__init__.py
+++ src/openlayer/lib/data/__init__.py
@@ -1,6 +1,7 @@
 """Data upload functions."""
 
-__all__ = ["upload_reference_dataframe", "StorageType"]
+__all__ = ["StorageType", "upload_reference_dataframe", "upload_batch_inferences"]
 
 from ._upload import StorageType
 from .reference_dataset import upload_reference_dataframe
+from .batch_inferences import upload_batch_inferences
