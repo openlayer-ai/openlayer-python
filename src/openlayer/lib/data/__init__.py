970f49aaf10d54e80f6b65e3fb5db0e224fe4e46
Rishab Ramanathan <rishramanathan@gmail.com>
2024-07-30 15:39:42 -0700

    improvement: include method to update batch of inferences

--- src/openlayer/lib/data/__init__.py
+++ src/openlayer/lib/data/__init__.py
@@ -1,7 +1,12 @@
 """Data upload functions."""
 
-__all__ = ["StorageType", "upload_reference_dataframe", "upload_batch_inferences"]
+__all__ = [
+    "StorageType",
+    "upload_reference_dataframe",
+    "upload_batch_inferences",
+    "update_batch_inferences",
+]
 
 from ._upload import StorageType
+from .batch_inferences import update_batch_inferences, upload_batch_inferences
 from .reference_dataset import upload_reference_dataframe
-from .batch_inferences import upload_batch_inferences
